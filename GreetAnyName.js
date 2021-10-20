ca42167efc71ba1726420a483bc33a0c80928ec5
Niteesh Kumar <88903346+shinchu-itachi@users.noreply.github.com>
2021-10-20 18:48:42 +0530

    Add files via upload

---
+++ GreetAnyName.js
@@ -0,0 +1,7 @@
+// we can use our name to greet our self and print a hello message for any name we can provide
+// here we will first directly print the name and then we will pass a name in template string in console with the dollar sign to print it
+
+console.log("Hello Shinch_itachi !!!!!")  //first method directly printing it
+
+const name = "Shinchu_itachi"
+console.log(`Hello ${name}`) //second method using variable and template string and using ${variable} to print it
