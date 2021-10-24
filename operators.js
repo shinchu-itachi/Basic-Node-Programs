7e73647422de63b60f9dcc01636a9855b8a77c6f
Niteesh Kumar <88903346+shinchu-itachi@users.noreply.github.com>
2021-10-24 16:43:59 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,32 @@
+// in this file / programm we will see some basic arithematic operation
+
+// simple add of two numbers
+let a = 10
+let b = 9
+let c = a + b 
+console.log(`The sum of ${a} and ${b} is ${c}`)
+
+// simple substraction of two numbers
+
+c = a - b 
+console.log(`The difference of ${a} and ${b} is ${c}`)
+
+//simple multiply
+
+c = a * b 
+console.log(`The multiplication of ${a} and ${b} is ${c}`)
+
+//simple division of the two numbers
+
+c = a / b 
+console.log(`The division of ${a} and ${b} is ${c}`)
+
+//simple module operation of two numbers
+
+c =  a % b
+console.log(`The remainder of ${a} divides ${b} is ${c}`)
+
+//power function in javascript a^b
+
+c = a ** b
+console.log(`The power of ${a} raise to ${b} is ${c}`)
